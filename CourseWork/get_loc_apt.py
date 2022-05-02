daae7a2e362a74ec98b320edab66ac6a28663744
Ilya Yakovchik <71668414+Accgen@users.noreply.github.com>
2022-05-03 02:04:40 +0300

    Update get_loc_apt.py

--- CourseWork/get_loc_apt.py
+++ CourseWork/get_loc_apt.py
@@ -1,6 +1,6 @@
 from geopy import Yandex
 while(True):
-    place = input("Введи адрес аптеки: ") #Сюда адрес апетки
+    place = input("Введи адрес аптеки: ") #Сюда адрес аптеки
     location = Yandex(api_key='0ef27e34-3ede-402a-95f2-fee33edd3e6f').geocode(place)
     lat = location.latitude
     lon = location.longitude
